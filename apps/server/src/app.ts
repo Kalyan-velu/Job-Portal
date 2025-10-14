b914d2e0c37e79cb4af67ff9f36b07379a7d9004
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2025-10-14 21:13:03 +0530

    (chore) refactor: added monorepo configuration

---
+++ apps/server/src/app.ts
@@ -0,0 +1,48 @@
+import 'dotenv/config'
+import express from 'express'
+
+import { middleware } from './common/middleware.config'
+import RouterConfigure from './common/routes.config'
+import { Applicant, ApplicantApplication } from './modules/applicants'
+import CompanyModule from './modules/company'
+import userModules from './modules/user'
+
+import database from './common/database.config'
+import { PrivateJobModule, PublicJobModule } from './modules/job'
+import path from 'node:path'
+
+const app = express()
+const configApi = new RouterConfigure(app)
+
+const mongoose = database.connectDB()
+middleware.configureMiddleware(app)
+middleware.attach({ name: 'mongoose', property: mongoose })
+
+const modules = [
+  Applicant,
+  ApplicantApplication,
+  userModules.Auth,
+  userModules.User,
+  PrivateJobModule,
+  PublicJobModule,
+  CompanyModule,
+]
+
+for (let module of modules) {
+  configApi.configureRoute(module)
+}
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(path.resolve(), '../web/dist')))
+
+  app.get('*', (request, response) => {
+    response.sendFile(path.resolve(path.resolve(), "../web", "dist", "index.html"))
+  })
+} else {
+  app.get("/", (request, response) => {
+    response.json({message: "Server is Up"});
+  });
+}
+
+
+export default app
